3ed77be73ce809f6e24df243f9764e9f2310db92
sean Y <sean.youngs@outlook.com>
2022-08-26 22:05:54 +0100

    First script started. Cannot work on player movement due to issue with asset manager

---
+++ EnemyMovementToPoints.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementToPoints : MonoBehaviour
+{
+    // arrays for points to move to
+    // Coroutines for pauses.
+
+    [SerializeField] private GameObject[] movePoints;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
