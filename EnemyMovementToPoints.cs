032f1bf0839f557c426cc5127327190a964a06de
sean Y <sean.youngs@outlook.com>
2022-08-28 21:03:40 +0100

    Movement working correctly between points. MoveTowards needs to be in an update function.

--- EnemyMovementToPoints.cs
+++ EnemyMovementToPoints.cs
@@ -10,69 +10,39 @@ public class EnemyMovementToPoints : MonoBehaviour
     [SerializeField] private float speed = 1.0f;
     [SerializeField] private float waitTime = 1.0f;
     private bool canMove = true;
+    private int movePointItem;
 
-    // lerp from currentPos, to movePont[x]
-    // if i =  movepoint[max], then i = 0.
-
-    // Start is called before the first frame update
     private void Start()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoints[1].transform.position, speed * Time.deltaTime);
-        //for (int i = 0; i <= movePoints.Length; i++)
-        //{
-        //    float step = speed * Time.deltaTime;
-        //    Debug.Log("Moving to movePoint " + i);
-        //    transform.position = Vector3.MoveTowards(transform.position, movePoints[i].transform.position, step);
-        //    if (i >= movePoints.Length)
-        //    {
-        //        Debug.Log("Resetting i to 0");
-        //        i = 0;
-        //    }
-        //    canMove = true;
-        //}
     }
 
     // Update is called once per frame
     private void Update()
     {
+        MoveObj(movePointItem);
+
         if (canMove)
         {
-            //StartCoroutine("Move");
+            StartCoroutine("Move");
             canMove = false;
-        }
-
-        //transform.position = Vector3.MoveTowards(transform.position, movePoints[1].transform.position, speed * Time.deltaTime);
-
-        for (int i = 0; i <= movePoints.Length; i++)
-        {
-            float step = speed * Time.deltaTime;
-            Debug.Log("Moving to movePoint " + i);
-            transform.position = Vector3.MoveTowards(transform.position, movePoints[i].transform.position, step);
-            //if (i >= movePoints.Length)
-            //{
-            //    Debug.Log("Resetting i to 0");
-            //    i = 0;
-            //}
         }
     }
 
     private IEnumerator Move()
     {
-        float step = speed * Time.deltaTime;
-        for (int i = 0; i <= movePoints.Length; i++)
+        for (int i = 0; i < movePoints.Length; i++)
         {
-            if (i > movePoints.Length)
-            {
-                Debug.Log("Resetting i to 0");
-                i = 0;
-            }
-            Debug.Log("Moving to movePoint " + i);
-            Debug.Log(movePoints[i]);
-            transform.position = Vector3.MoveTowards(transform.position, movePoints[i].transform.position, step);
-            yield return new WaitForSeconds(waitTime);
-
-            canMove = true;
-            yield return null;
+            movePointItem = i;
+            Debug.Log(movePointItem);
+            float timeToWait = speed + waitTime;
+            yield return new WaitForSeconds(timeToWait);
         }
+        canMove = true;
+        yield return null;
+    }
+
+    private void MoveObj(int item)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoints[item].transform.position, speed * Time.deltaTime);
     }
 }
