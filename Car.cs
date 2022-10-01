74bfd9f0164bdc359e362cc71ca5261c1d7a3ac7
sean Y <sean.youngs@outlook.com>
2022-10-01 16:13:45 +0100

    Worked more on car and added script for pause menu.

--- Car.cs
+++ Car.cs
@@ -9,6 +9,7 @@ public class Car : MonoBehaviour
     private Player player;
 
     [SerializeField] private float speed = 10f;
+    [SerializeField] private float maxRotation = 5f;
     private Vector3 moveVector;
 
     // Start is called before the first frame update
@@ -21,7 +22,13 @@ public class Car : MonoBehaviour
     private void Update()
     {
         moveVector.x = player.GetAxis("Move Horizontal");
-
+        transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
         // max rotation -5 to 5 on y
+
+        Vector3 currentRotation = gameObject.transform.rotation.eulerAngles;
+        if (moveVector.x != 0.0f && currentRotation.y <= maxRotation && currentRotation.y >= -maxRotation)
+        {
+            gameObject.transform.rotation = new Quaternion(0, moveVector.x, 0, 1);
+        }
     }
 }
