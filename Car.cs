2c45aa0df95b6fb900b0b5abb1f1356b5905a600
sean Y <sean.youngs@outlook.com>
2022-10-04 19:09:17 +0100

    Added code for car movement

--- Car.cs
+++ Car.cs
@@ -10,32 +10,49 @@ public class Car : MonoBehaviour
 
     [SerializeField] private float speed = 10f;
     [SerializeField] private float maxRotation = 5f;
+    [SerializeField] private float turnSpeed = 5f;
+    [SerializeField] private float turnClamp = 5f;
     private Vector3 moveVector;
-    public Transform target;
+    private Rigidbody rb;
+    private float yPos;
 
     // Start is called before the first frame update
     private void Start()
     {
         player = ReInput.players.GetPlayer(playerID);
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
         moveVector.x = player.GetAxis("Move Horizontal");
-        //transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
-        // max rotation -5 to 5 on y
-        Debug.Log(moveVector.x);
-        Vector3 currentRotation = gameObject.transform.rotation.eulerAngles;
+        yPos = transform.position.y;
 
-        if (moveVector.x != 0.0f && currentRotation.y <= maxRotation && currentRotation.y >= -maxRotation)
+        // REDO all of below. Start with side to side movement and add the rotation in later.
+
+        //transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+        float turnVector = moveVector.x * turnSpeed;
+
+        //float clampX = Mathf.Clamp(transform.position.x, -turnClamp, turnClamp);
+        //Debug.Log(clampX);
+        // transform.position += new Vector3(turnVector, 0, 0) * Time.deltaTime;
+        //Debug.Log(transform.position.x);
+        if (transform.position.x < turnClamp + 1 && transform.position.x > -turnClamp - 1 && moveVector.x != 0.0f)
         {
-            gameObject.transform.Rotate(0, moveVector.x, 0, Space.World);
+            rb.AddForce(new Vector3(turnVector, 0, 0));
         }
-        if (moveVector.x <= 0.0f && currentRotation.y > maxRotation)
+        if (transform.position.x > turnClamp)
         {
-            float rotateBack = currentRotation.y - maxRotation;
-            gameObject.transform.Rotate(0, rotateBack, 0, Space.World);
+            Debug.Log("went over clamp");
+            float moveBack = transform.position.x - 0.2f;
+            transform.position = new Vector3(turnClamp, yPos, 0);
+        }
+        if (transform.position.x < -turnClamp)
+        {
+            Debug.Log("went under clamp");
+            float moveBack = transform.position.x + 0.2f;
+            transform.position = new Vector3(-turnClamp, yPos, 0);
         }
     }
 }
