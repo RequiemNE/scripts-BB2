f7fad88b3d3210292076a5ff5688f236bc2cd2d1
sean Y <sean.youngs@outlook.com>
2022-10-30 17:39:44 +0000

    added more logic to enemy spawn

--- EnemySpawns.cs
+++ EnemySpawns.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class EnemySpawns : MonoBehaviour
 {
     [SerializeField] private GameObject[] spawnPoints;
+    [SerializeField] private GameObject enemy;
+
+    private const float COUNTDOWN1 = 5f;
+    private const float COUNTDOWN2 = 8f;
+    private const float COUNTDOWN3 = 2f;
+
+    private bool spawnEnemy = true;
 
     // Start is called before the first frame update
     private void Start()
@@ -14,5 +21,49 @@ public class EnemySpawns : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // countdown -= time.deltaTime
+        // when time = 0 spawn enemy.
+        // reset countdown.
+        if (spawnEnemy)
+        {
+            TimerChoice();
+        }
+    }
+
+    private void TimerChoice()
+    {
+        spawnEnemy = false;
+        Debug.Log("in TimerChoice");
+        float randomChoice = Random.Range(0, 2);
+        Debug.Log("Timer rand :" + randomChoice);
+
+        switch (randomChoice)
+        {
+            case 0:
+                SpawnEnemy(COUNTDOWN1);
+                break;
+
+            case 1:
+                SpawnEnemy(COUNTDOWN2);
+                break;
+
+            case 2:
+                SpawnEnemy(COUNTDOWN3);
+                break;
+        }
+    }
+
+    private void SpawnEnemy(float countdown)
+    {
+        Debug.Log("in countdown");
+        float timer = countdown;
+        //while (timer != 0f)
+        //{
+        //   timer -= Time.deltaTime;
+        //}
+
+        int randomSpawn = Random.Range(0, 2);
+        Debug.Log("spawn rand :" + randomSpawn);
+        Instantiate(enemy, spawnPoints[randomSpawn].transform);
     }
 }
