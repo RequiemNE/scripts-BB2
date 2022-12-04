9e7188b2e149fefadc24768db602420cfad81ed6
sean Y <sean.youngs@outlook.com>
2022-12-04 17:03:59 +0000

    First iteration of game settings

---
+++ SettingsMenu.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Dropdown resolutionDropdown;
+    [SerializeField] private Slider volumeSlider;
+
+    private float currentVolume;
+    private Resolution[] resolutions;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+        currentVolume = volume;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetFloat("VolumePreference", currentVolume);
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("VolumePreference"))
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
+        }
+        else
+        {
+            volumeSlider.value = volumeSlider.maxValue / 2;
+        }
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+        {
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        }
+        else
+        {
+            resolutionDropdown.value = currentResolutionIndex;
+        }
+    }
+}
