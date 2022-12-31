1ebd960cccffbf6e0bc957a26ac118455c87829f
sean Y <sean.youngs@outlook.com>
2022-12-31 19:53:42 +0000

    expanded pause menu

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Rewired;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject settingsButton;
+    [SerializeField] private GameObject resumeButton;
+    [SerializeField] private GameObject mainMenuButton;
 
     public int playerID = 0;
     private Player player;
@@ -18,31 +22,64 @@ public class PauseMenu : MonoBehaviour
     private void Start()
     {
         player = ReInput.players.GetPlayer(playerID);
+        pauseMenu.SetActive(false);
+        settings.SetActive(false);
     }
 
     // Update is called once per frame
     private void Update()
     {
         pauseInput = player.GetButtonDown("Pause");
+        Pause();
     }
 
     private void Pause()
     {
+        if (pauseInput)
+        {
+            if (pause == false)
+            {
+                pause = true;
+            }
+            else
+            {
+                pause = false;
+            }
+        }
         if (pause)
         {
+            Debug.Log("pause");
             // stop player and enemies.
             // load menu
             // settings false.
+            pauseMenu.SetActive(true);
+            settings.SetActive(false);
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            settings.SetActive(false);
         }
     }
 
-    private void MainMenu()
+    // BUTTON FUCNTIONS -------------------------
+
+    public void MainMenu()
     {
-        // load main menu
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 
-    private void Settings()
+    public void Settings()
     {
         // root false
+        pauseMenu.SetActive(false);
+        settings.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        settings.SetActive(true);
+        // resume elements.
     }
 }
